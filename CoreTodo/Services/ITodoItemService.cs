b39c2645da21717489ecd318601de8da9e465a0e
Iain Stewart <iain.stewart@mcdermottgroup.co.uk>
2018-07-16 14:14:23 +0100

    Create interface for TodoItemService

---
+++ CoreTodo/Services/ITodoItemService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreTodo.Models;
+
+namespace CoreTodo.Services
+{
+    public interface ITodoItemService
+    {
+        Task<TodoItem[]> GetIncompleteItemsAsync();
+    }
+}
